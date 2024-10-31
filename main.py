0f38a8035ea27119c4bd37dd952fc76ae915a1c6
Duncan McFarlane <dmcf2@pdx.edu>
2024-10-30 17:56:57 -0700

    Added a basic main file, this has a few imports for libraries I'm expecting us to use

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+if sys.version_info < (3, 8):
+    raise RuntimeError("This program requires Python 3.8 or higher")
+import os               # File IO
+import sqlite3          # Database
+import re               # Regex (idx might come in handy, it's good to have too many then too little)
+import logging          # Nice interface for logging, just captures timestamp and formats nice
+from dataclasses import dataclass, field # Class helpers, py 3.8+ features that make classes much shorter to write
+from typing import *    # Type hinting
+
+import pytest
+
+def main():
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
