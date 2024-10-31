f9c63fac17f1e76d4dedbb9cff5821005c28b1e4
Marcos Chavez <mchav2@pdx.edu>
2024-10-30 18:21:38 -0700

    Removed pytest and updated the database import for postgres

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ import sys
 if sys.version_info < (3, 8):
     raise RuntimeError("This program requires Python 3.8 or higher")
 import os               # File IO
-import sqlite3          # Database
+import psycopg2          # Database
 import re               # Regex (idx might come in handy, it's good to have too many then too little)
 import logging          # Nice interface for logging, just captures timestamp and formats nice
 from dataclasses import dataclass, field # Class helpers, py 3.8+ features that make classes much shorter to write
 from typing import *    # Type hinting
-
-import pytest
 
 def main():
     return 0
