a9895081f0bcfae36f31daf19892758925530aa3
Marcos Chavez <mchav2@pdx.edu>
2024-11-18 21:55:26 -0800

    finished the classes from the design documents. Added a menu.py class for menu manager class

--- classes.py
+++ classes.py
@@ -23,19 +23,24 @@ class Member:
     def validate(self):
         pass
     
-    """updates the members status
-    """
-    def updateStatus(newStatus):
+    #updates the members status
+    def updateStatus(self,newStatus):
         pass
     
     #displays member information
-    def displayInfo():
+    def displayInfo(self):
+        print("Member Number:",self._memberNumber)
+        print("First Name:",self._firstName)
+        print("Last Name",self._lastName)
+        print("Address:",self._streetAddress,self._city,self._state,self._zipCode)
+        print("Membership Status:",self._status)
         pass
 
 class Provider:
     def __init__(self) -> None:
         self._providerNumber:str = None
         self._name:str = None
+        self._streetAddress:str = None
         self._city:str = None
         self._state:str = None
         self._zipCode:str = None
@@ -62,7 +67,9 @@ class Provider:
         pass
 
     def displayInfo(self):
-
+        print("Provider Number:",self._providerNumber)
+        print("Name:",self._name)
+        print("Address:",self._streetAddress,self._city,self._state,self._zipCode)
         pass
 
 class Service:
@@ -74,6 +81,9 @@ class Service:
 
     #show service details
     def displayInfo(self):
+        print("Service Name:",self._serviceName)
+        print("Service Code:",self._serviceCode)
+        print("Fee:",self._fee)
         pass
 
 class ServiceRecord:
@@ -99,5 +109,85 @@ class ServiceRecord:
     #displays the service record details
     def displayInfo(self):
         print("Record ID:",self._recordID)
+        print("Date Received:",self._dateReceived)
+        print("Provider:",self._provider)
+        print("Member:",self._member)
+        print("Comments:",self._comments)
+        print("Fee:",self._fee)
+        pass
 
+class ChocAnSystem:
+    def __init__(self) -> None:
+        #list of members objects
+        self._members = None
+        #list of Provider objects
+        self._providers = None
+        #list of service objects
+        self._services = None
+        #list of service Record objects
+        self._serviceRecords = None
+        pass
+
+    def validateMember(self):
+        pass
+
+    def validateProvider(self):
+        pass
+
+    def addMember(self):
+        pass
+    
+    #updates member information
+    def updateMember(self):
+        pass
+
+    def deleteMember(self):
+        pass
+
+    #adds new provider
+    def addProvider(self):
+        pass
+
+    #updates provider information
+    def updateProvider(self):
+        pass
+
+    #removes a provider
+    def deleteProvider(self):
+        pass
+    
+    #adds a new service record
+    def addServiceRecord(self):
+        pass
+
+    #Generates all required weekly reports
+    def generateWeeklyReports(self):
+        pass
+
+    #updates member statuses based on payment
+    def processMembershipPayments(self):
+        pass
+
+    #retrieves the list of services for provider
+    def getProviderDirectory(self):
+        pass
+
+class ReportGenerator:
+    def __init__(self) -> None:
+        pass
+
+    #creates a report for a specific member
+    def generateMemberReport(self):
+        pass
+
+    #creates a report for a specific provider
+    def generateProviderReport(self):
+        pass
+
+    #generates eft data for provider payments
+    def generateEFTData(self):
+        pass
+
+    #creates the provider directory file
+    def generateProviderDirectory(self):
         pass
--- main.py
+++ main.py
@@ -2,13 +2,25 @@ import sys
 if sys.version_info < (3, 8):
     raise RuntimeError("This program requires Python 3.8 or higher")
 import os               # File IO
-import psycopg2          # Database
 import re               # Regex (idx might come in handy, it's good to have too many then too little)
 import logging          # Nice interface for logging, just captures timestamp and formats nice
 from dataclasses import dataclass, field # Class helpers, py 3.8+ features that make classes much shorter to write
 from typing import *    # Type hinting
 
 def main():
+
+    print("select what mode you want")
+    print("1.Operator mode")
+    print("2. Provider Mode")
+    mode = input()
+
+    #operator mode
+    if mode == 1:
+        member_num = input("enter you membership number")
+    #provider mode
+    if mode == 2:
+        provider_num = input("enter you provider number")
+
     return 0
 
 if __name__ == "__main__":
